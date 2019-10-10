918f2946e9c9f4d374b0b8385d2fa9a3894847de
Ben <allenben@protonmail.com>
2019-10-10 11:29:00 -0500

    Created facebook auth set-up

---
+++ auth/facebookAuth.js
@@ -0,0 +1,8 @@
+const passport = require('passport');
+const express = require('express');
+const FacebookAuth = require('passport-facebook');
+
+
+//Retrived from https://developers.facebook.com/apps/2395844084004101/settings/basic/
+const FACEBOOK_APP_ID = '2395844084004101';
+const FACEBOOK_APP_SECRET = '53800821ec15471c0ecd621831f2bd25';
