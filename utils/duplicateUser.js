deffe1483e33d0c4401cd31bda9c9d2344e66d50
Eli Sacks <bastlifa@gmail.com>
2019-10-10 12:57:58 -0400

    docs for auth, validation for auth

---
+++ utils/duplicateUser.js
@@ -0,0 +1,16 @@
+const Users = require('../users/usersModel')
+
+module.exports = (req, res, next) =>
+{
+    const email = req.body.email
+    Users.findBy({email})
+        .then(response =>
+            {
+                if (response.id) res.status(409).json({ message: 'A user with that email already exists' })
+                else next()
+            })
+        // .catch(err =>
+        //     {
+        //         next()
+        //     })
+}
