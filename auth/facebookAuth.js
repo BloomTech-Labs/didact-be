977e3d5caf2c06eff0a5471778a72341f926122f
AndrewA0112 <andrew.mkal@outlook.com>
2019-10-11 13:33:46 -0500

    Added secrets for facebookAuth

--- auth/facebookAuth.js
+++ auth/facebookAuth.js
@@ -2,21 +2,19 @@ const passport = require('passport');
 const FacebookAuth = require('passport-facebook');
 const router = require('express').Router()
 
-
-//Retrived from https://developers.facebook.com/apps/2395844084004101/settings/basic/
-const FACEBOOK_APP_ID = '2395844084004101';
-const FACEBOOK_APP_SECRET = '53800821ec15471c0ecd621831f2bd25';
+const secrets = require('../config/secret')
 
 passport.use(new FacebookAuth({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "https://didactlms-staging.herokuapp.com/api/auth/facebook/callback"
+    clientID: secrets.facebookId,
+    clientSecret: secrets.facebookSecret,
+    callbackURL: `${secrets.passportUrl}/api/auth/facebook/callback`
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log("Authed")
     cb(null, profile)
   }
 ));
+console.log(secrets.passportUrl)
 
 passport.serializeUser(function(user, cb) {
     cb(null, user);
@@ -29,7 +27,7 @@ passport.deserializeUser(function(user, cb) {
 router.use(passport.initialize());
 router.use(passport.session());
 
-router.get('/', passport.authenticate('facebook', { callbackURL: 'https://didactlms-staging.herokuapp.com/api/auth/facebook/callback'}))
+router.get('/', passport.authenticate('facebook', { callbackURL: `${secrets.passportUrl}/api/auth/facebook/callback`}))
 
 router.get('/callback',
   passport.authenticate('facebook', { failureRedirect: '/login'}),
