98b148103147df3acf310b6afd72f868ca9da2d7
Eli Sacks <bastlifa@gmail.com>
2019-10-28 17:31:27 -0400

    learning path endpoint

--- learning-paths/learningPathsRouter.js
+++ learning-paths/learningPathsRouter.js
@@ -96,4 +96,69 @@ router.get('/', (req, res) => {
         })
 })
 
+
+/**
+ * @api {get} /api/learning-paths/:id Get Learning Path
+ * @apiName GetLearningPath
+ * @apiGroup Learning Paths
+ * 
+ * @apiHeader {string} Content-Type the type of content being sent
+ * @apiHeader {string} token User's token for authorization
+ * 
+ * @apiHeaderExample {json} Header-Example:
+ * {
+ *  "Content-Type": "application/json",
+ *  "authorization": "sjvbhoi8uh87hfv8ogbo8iugy387gfofebcvudfbvouydyhf8377fg"
+ * }
+ * 
+ * @apiSuccess (200) {Object} Learning Path An object of the Learning Path
+ * 
+ * @apiSuccessExample Success-Response:
+ * HTTP/1.1 200 OK
+ * 
+ * {
+ *     "id": 1,
+ *     "name": "Onboarding Learning Path",
+ *     "description": "This learning path will get you on the road to success.",
+ *     "category": "Learning"
+ * }
+ * 
+ * @apiError (401) {Object} bad-request-error The authorization header is absent
+ * 
+ * @apiErrorExample 401-Error-Response:
+ * HTTP/1.1 401 Bad Request
+ * {
+ *  "message": "Forbidden Access!"
+ * }
+ * 
+ * @apiError (401) {Object} bad-request-error The authorization is invalid
+ * 
+ * @apiErrorExample 401-Error-Response:
+ * HTTP/1.1 401 Bad Request
+ * {
+ *  "message": "Invalid Credentials"
+ * }
+ * 
+ * @apiError (500) {Object} internal-server-error Could not retrieve learning path
+ * 
+ * @apiErrorExample 500-Error-Response:
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *  "message": "Error connecting with server"
+ * }
+ * 
+ */
+
+router.get('/:id', (req, res) => {
+    Paths.findById(req.params.id)
+    .then(response => 
+        {
+            res.status(200).json(response)
+        })
+    .catch(error => 
+        {
+            res.status(500).json({ message: 'Error connecting with server' })
+        })
+})
+
 module.exports = router
