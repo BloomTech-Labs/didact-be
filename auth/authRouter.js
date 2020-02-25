0a1c3758e1e0552506751922519c0b2b525a4a5e
nicanor-alicaya <donalicaya@gmail.com>
2020-02-25 12:46:13 -0500

    added routes to do a get/put request for users in authRouter.js and added find/update model in usersModel.js

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -10,29 +10,61 @@ const secrets = require('../config/secret')
 
 const Users = require('../users/usersModel')
 
-router.post('/emaillist', (req, res) =>
-{
-    if(!req.body.email) res.status(400).json({ message: 'Must send email' })
-    else
-    {
-        let email = req.body.email
-        Users.checkEmailListForEmail(email)
-        .then(emailResponse =>
-        {
-            console.log(emailResponse)
-            if(emailResponse) res.status(200).json({ message: 'Email was already in database' })
-            else 
-            {
-                Users.addToEmailList(email)
-                .then(response => res.status(201).json({ message: 'Email has been added to list' }))
-                .catch(err => res.status(500).json({ message: 'Internal Error: Could not add email' }))
+//GET list of all users
+router.get("/users", (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            console.log(
+                err,
+                "credentials are invalid or missing"
+            );
+            res
+                .status(500)
+                .json({ error: "Error unable to retrieve list of users" });
+        });
+});
+
+//UPDATE user by specific id
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    Users.update(id, changes)
+        .then(user => {
+            if (user) {
+                res.json({ update: user });
+            } else {
+                res
+                    .status(404)
+                    .json({ message: `Could not find user with id:${id}` });
             }
         })
-        .catch(error =>
-        {
-            
-            res.status(500).json({ message: 'Internal Error: Could not add email' })
-        })
+        .catch(err => {
+            res.status(500).json({ message: "Failed to update user" });
+        });
+});
+
+router.post('/emaillist', (req, res) => {
+    if (!req.body.email) res.status(400).json({ message: 'Must send email' })
+    else {
+        let email = req.body.email
+        Users.checkEmailListForEmail(email)
+            .then(emailResponse => {
+                console.log(emailResponse)
+                if (emailResponse) res.status(200).json({ message: 'Email was already in database' })
+                else {
+                    Users.addToEmailList(email)
+                        .then(response => res.status(201).json({ message: 'Email has been added to list' }))
+                        .catch(err => res.status(500).json({ message: 'Internal Error: Could not add email' }))
+                }
+            })
+            .catch(error => {
+
+                res.status(500).json({ message: 'Internal Error: Could not add email' })
+            })
     }
 })
 
@@ -59,7 +91,7 @@ router.post('/register', validateUserRegister, duplicateUser, (req, res) => {
                 .then(user => {
                     if (user && bcrypt.compareSync(originalPass, user.password)) {
                         const token = generateToken(user)
-                        res.status(201).json({ token, id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name });
+                        res.status(201).json({ token, user });
                     } else {
                         res.status(401).json({ message: 'Invalid Credentials' });
                     }
@@ -71,7 +103,7 @@ router.post('/register', validateUserRegister, duplicateUser, (req, res) => {
         })
         .catch(error => {
             console.log(error)
-            res.status(500).json({ message: 'Error connecting with server'})
+            res.status(500).json({ message: 'Error connecting with server' })
         })
 })
 
@@ -82,7 +114,7 @@ router.post('/login', validateUserLogin, (req, res) => {
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
                 const token = generateToken(user)
-                res.status(200).json({ token, id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name});
+                res.status(200).json({ token, user });
             } else {
                 res.status(401).json({ message: 'Invalid Credentials' });
             }
@@ -99,16 +131,13 @@ router.post('/', (req, res) => {
     if (token) {
         jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
             if (err) res.status(401).json({ message: 'Token does not exist' })
-            else 
-            {
-                Users.findBy({email: decodedToken.email})
-                .then(user =>
-                    {
-                        if(user) res.status(200).json({email: decodedToken.email, id: user.id, photo: user.photo || null, first_name: user.first_name, last_name: user.last_name })
+            else {
+                Users.findBy({ email: decodedToken.email })
+                    .then(user => {
+                        if (user) res.status(200).json({ email: decodedToken.email, id: user.id, photo: user.photo || null, first_name: user.first_name, last_name: user.last_name })
                         else res.status(404).json({ message: 'No such user found' })
                     })
-                .catch(err =>
-                    {
+                    .catch(err => {
                         res.status(500).json({ message: 'Internal server error, could not retrieve user' })
                     })
             }
@@ -118,16 +147,14 @@ router.post('/', (req, res) => {
     }
 })
 
-router.post('/contactmessage', restricted, (req, res) => 
-{
-    if(!req.body.message || !req.body.email || !req.body.name) res.status(400).json({ message: 'body must include name, email, and message' })
-    else
-    {
+router.post('/contactmessage', restricted, (req, res) => {
+    if (!req.body.message || !req.body.email || !req.body.name) res.status(400).json({ message: 'body must include name, email, and message' })
+    else {
         let name = req.body.name
         let email = req.body.email
         let message = req.body.message
 
-        let emailMessage = 
+        let emailMessage =
         {
             from: `${email}`,
             to: `${process.env.EMAIL}`,
@@ -136,18 +163,16 @@ router.post('/contactmessage', restricted, (req, res) =>
             // html: `<p>${message}</p>`
         }
 
-        
+
         sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-        
+
         sgMail.send(emailMessage)
-        .then(response => 
-        {
-            res.status(201).json({ message: 'Email Sent' })
-        })
-        .catch(error =>
-        {
-            res.status(500).json({ message: 'Error sending message' })
-        })
+            .then(response => {
+                res.status(201).json({ message: 'Email Sent' })
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'Error sending message' })
+            })
     }
 })
 
@@ -159,21 +184,19 @@ function generateToken(user) {
     const options = {
         expiresIn: '7d'
     }
-    
+
     return jwt.sign(payload, secrets.jwtSecret, options)
 }
 
-function validateUserRegister(req, res, next)
-{
-    if(!req.body) res.status(400).json({ message: "missing user data"})
-    else if(!req.body.email || !req.body.password || !req.body.first_name || !req.body.last_name) res.status(400).json({ message: "email, password, first_name, and last_name are required"})
+function validateUserRegister(req, res, next) {
+    if (!req.body) res.status(400).json({ message: "missing user data" })
+    else if (!req.body.email || !req.body.password || !req.body.first_name || !req.body.last_name) res.status(400).json({ message: "email, password, first_name, and last_name are required" })
     else next()
 }
 
-function validateUserLogin(req, res, next)
-{
-    if(!req.body) res.status(400).json({ message: "missing user data"})
-    else if(!req.body.email || !req.body.password) res.status(400).json({ message: "missing email or password"})
+function validateUserLogin(req, res, next) {
+    if (!req.body) res.status(400).json({ message: "missing user data" })
+    else if (!req.body.email || !req.body.password) res.status(400).json({ message: "missing email or password" })
     else next()
 }
 
--- knexfile.js
+++ knexfile.js
@@ -1,64 +1,51 @@
-// Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
+	development: {
+		client: "pg",
+		connection: process.env.DB_URL,
+		useNullAsDefault: true,
+		migrations: { directory: "./database/migrations" },
+		seeds: { directory: "./database/seeds" }
+	},
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/didact.db3'
-    },
-    useNullAsDefault: true,
-    pool:
-    {
-      afterCreate: (conn, done) =>
-      {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      },
-    },
-    migrations: {directory: './database/migrations'},
-    seeds: {directory: './database/seeds'},
-  },
+	testing: {
+		client: "sqlite3",
+		connection: {
+			filename: ":memory:"
+		},
+		useNullAsDefault: true,
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run("PRAGMA foreign_keys = ON", done);
+			}
+		},
+		migrations: { directory: "./database/migrations" },
+		seeds: { directory: "./database/seeds" }
+	},
 
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: ':memory:'
-    },
-    useNullAsDefault: true,
-    pool:
-    {
-      afterCreate: (conn, done) =>
-      {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      },
-    },
-    migrations: {directory: './database/migrations'},
-    seeds: {directory: './database/seeds'},
-  },
+	staging: {
+		client: "postgresql",
+		connection: {
+			database: "my_db",
+			user: "username",
+			password: "password"
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: "knex_migrations"
+		}
+	},
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: process.env.HEROKU_POSTGRESQL_COBALT_URL || process.env.DATABASE_URL,
-    pool: {
-    },
-    migrations: {directory: './database/migrations'},
-    seeds: {directory: './database/seeds'},
-  }
-
+	production: {
+		client: "pg",
+		connection:
+			process.env.HEROKU_POSTGRESQL_COBALT_URL || process.env.DATABASE_URL,
+		pool: {},
+		migrations: { directory: "./database/migrations" },
+		seeds: { directory: "./database/seeds" }
+	}
 };
