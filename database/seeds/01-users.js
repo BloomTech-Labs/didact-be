45134caaade033be9da333bc63ff1105cec4a68b
nicanor-alicaya <donalicaya@gmail.com>
2020-03-24 20:15:32 -0400

    added more seeds to users with corresponding profiles there are now 14 in total

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,31 +1,100 @@
-const bcryptjs = require('bcryptjs')
-const hashCount = require('../../utils/hashCount')
-const secrets = require('../../config/secret.js');
-exports.seed = function (knex, Promise) {
-    return knex('users')
-        .insert(
-            [
-
-                {
-                    email: "didactlms@gmail.com",
-                    first_name: "Mark",
-                    last_name: "Dudlik",
-                    password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
-                    owner: true
-                }, {
-                    email: "bob@bobmail.com",
-                    first_name: "bob",
-                    last_name: "bobson",
-                    password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
-                    admin: true
-                },
-                {
-                    email: "amy@example.com",
-                    first_name: "amy",
-                    last_name: "lee",
-                    password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
-                    moderator: true
-                }
-            ]
-        )
-}
+const bcryptjs = require("bcryptjs");
+const hashCount = require("../../utils/hashCount");
+const secrets = require("../../config/secret.js");
+exports.seed = function(knex, Promise) {
+  return knex("users").insert([
+    {
+      email: "didactlms@gmail.com",
+      first_name: "Mark",
+      last_name: "Dudlik",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      owner: true
+    },
+    {
+      email: "bob@bobmail.com",
+      first_name: "Bob",
+      last_name: "Bobson",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      admin: true
+    },
+    {
+      email: "amy@example.com",
+      first_name: "Amy",
+      last_name: "Lee",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      moderator: true
+    },
+    {
+      email: "owner@gmail.com",
+      first_name: "Tom",
+      last_name: "Boss",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      owner: true
+    },
+    {
+      email: "jd@gmail.com",
+      first_name: "John",
+      last_name: "Smith",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      moderator: true
+    },
+    {
+      email: "stephen@gmail.com",
+      first_name: "Stephen",
+      last_name: "Chow",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      moderator: true
+    },
+    {
+      email: "alice@gmail.com",
+      first_name: "Alice",
+      last_name: "Childs",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      moderator: true
+    },
+    {
+      email: "carlos@gmail.com",
+      first_name: "Carlos",
+      last_name: "Sanchez",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      admin: true
+    },
+    {
+      email: "ziggy@gmail.com",
+      first_name: "Seigfreid",
+      last_name: "Nosche",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount),
+      admin: true
+    },
+    {
+      email: "isabel@gmail.com",
+      first_name: "Isabel",
+      last_name: "Ortega",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount)
+    },
+    {
+      email: "suzzette@gmail.com",
+      first_name: "Suzzette",
+      last_name: "Arnando",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount)
+    },
+    {
+      email: "felice@gmail.com",
+      first_name: "Felicity",
+      last_name: "Black",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount)
+    },
+    {
+      email: "patrick@gmail.com",
+      first_name: "Patrick",
+      last_name: "Fitzsimmons",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount)
+    },
+    {
+      email: "michael@gmail.com",
+      first_name: "Michael",
+      last_name: "Takashi",
+      password: bcryptjs.hashSync(secrets.adminSecret, hashCount)
+    }
+  ]);
+};
--- database/seeds/01.5-user_profile.js
+++ database/seeds/01.5-user_profile.js
@@ -17,6 +17,72 @@ exports.seed = function(knex, Promise) {
       image:
         "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
       bio: "About Amy...."
+    },
+    {
+      user_id: 4,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Tom...."
+    },
+    {
+      user_id: 5,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About John...."
+    },
+    {
+      user_id: 6,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Stephen...."
+    },
+    {
+      user_id: 7,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Alice...."
+    },
+    {
+      user_id: 8,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Carlos...."
+    },
+    {
+      user_id: 9,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Seigfreid...."
+    },
+    {
+      user_id: 10,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Isabel..."
+    },
+    {
+      user_id: 11,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Suzzette..."
+    },
+    {
+      user_id: 12,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Felicity..."
+    },
+    {
+      user_id: 13,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Patrick..."
+    },
+    {
+      user_id: 14,
+      image:
+        "https://res.cloudinary.com/klawd/image/upload/v1584550569/wq3oxtstbdkg8s9jxuhb.png",
+      bio: "About Michael..."
     }
   ]);
 };
